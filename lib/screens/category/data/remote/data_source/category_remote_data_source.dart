c9ccd96ff0f6e89cf0c91d87214c2184b8ffda0b
Your Name <you@example.com>
2023-02-07 03:03:59 +0200

    rename foldere

--- lib/screens/category/data/remote/data_source/category_remote_data_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dashboard/screens/category/domain/model/category_entity.dart';
-
-abstract class CategoryRemoteDataSource {
-  Future<void> addCategory(String category);
-  Stream<List<CategoryEntity>> getCategory();
-  Future<void> deleteCategory(String categoryId);
-}
--- lib/screens/category/domain/repository/category_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dashboard/screens/category/domain/model/category_entity.dart';
-
-abstract class CategoryRepository {
-  Future<void> addCategory(String category);
-  Stream<List<CategoryEntity>> getCategory();
-  Future<void> deleteCategory(String categoryId);
-}
